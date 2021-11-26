ef09f88e1c2aab077d1c894e133e932e32dcd89e
Van Truong <school0123456@gmail.com>
2021-11-27 01:29:31 +0700

    Add TeachingAssignmentView for admin view

--- my_app/admin.py
+++ my_app/admin.py
@@ -387,93 +387,11 @@ class PersonalInfoView(MyBaseView):
 
 # 		return super(PersonalInfoView, self).render(template, **kwargs)
 
-# class TeachingAssignmentView(MyBaseView):
-# 	column_list = ('subject','teacher.user.full_name', 'classInfo', 'semester', 'school_year')
-# 	# column_extra_row_actions = [
-# 	# 	EndpointLinkRowAction('ti ti-pencil', '.function'),
-# 	# ]
-# 	column_extra_row_actions = [# Add a new action button
-# 		TemplateLinkRowAction("custom_row_actions.copy_row", "Show List Students"),
-# 	]
-# 	can_edit = False
-# 	can_delete = False
+class TeachingAssignmentView(MyBaseView):
+	column_list = ('subject','teacher.user.full_name', 'classInfo', 'semester', 'school_year')
+	
+	create_template = 'admin/create_teaching_assignment.html'
 
-# 	@expose('/function')
-# 	def copy_view(self):
-# 		pass
-# 	def get_query(self):
-# 		if current_user.is_teacher:
-# 			teacher_id = Teacher.query.filter_by(user_id=current_user.user_id).first().id
-# 			return self.session.query(self.model).filter(self.model.teacher_id == teacher_id)
-# 		else:
-# 			return super(TeachingAssignmentView,self).get_query(self)
-
-# 	list_template = 'teacher/list_class.html'
-# 	@expose('/')
-# 	def info_view(self):
-# 		return super(TeachingAssignmentView,self).list_view()
-
-
-# class StudentInClassView_Teacher(MyBaseView):
-# 	column_list = ('student','student.user.full_name', 'classInfo', 'classInfo.school_year')
-# 	can_delete = False
-# 	def get_query(self):
-# 		cid = False if request.args.get('cid') is None else request.args.get('cid')
-# 		if cid != False:
-# 			return self.session.query(self.model).filter(self.model.class_info_id == int(cid))
-# 		else:
-# 			return self.session.query(self.model)
-
-# 	list_template='teacher/list_students.html'
-# 	@expose('/class')
-# 	def list_students(self):
-# 		self._template_args['teaching_id'] = request.args.get('teaching-id')
-# 		self._template_args['cid'] = request.args.get('cid')		
-# 		return super(StudentInClassView_Teacher,self).index_view()
-
-
-# class SubjectTranscriptView_Teacher(MyBaseView):
-# 	column_list = ('student','transcript_details')
-# 	can_delete = False
-# 	list_template = 'teacher/edit_score.html'
-
-
-# 	def get_query(self):
-# 		action = False if request.args.get('action') is None else request.args.get('action')
-# 		teaching_id = False if request.args.get('teaching-id') is None else request.args.get('teaching-id')
-# 		if action != False and action == 'edit':
-# 			return self.session.query(self.model).filter(self.model.transcript_info_id == int(teaching_id))
-# 		else:
-# 			return self.session.query(self.model)
-
-# 	def get_field(self,field):
-# 		if field.fieldtype == "float":
-# 			return fields.FloatField(field.label)
-
-# 	@expose('/class/score')
-# 	def score_view(self):
-# 		class DynamicForm(FlaskForm):pass
-
-# 		# dform = self.models.Form.objects.get(name='scoreType')
-# 		score_type = [(type.id, type.score_name) for type in ScoreType.query.all()]
-
-# 		for item in score_type:
-# 			setattr(DynamicForm, str(item[0]), fields.FloatField(item[1]))
-# 		form = DynamicForm()
-# 		# score_type = [{"score.label": type.score_name} for type in ScoreType.query.all()]
-# 		# form = TranscriptForm(transcripts=score_type)
-# 		action = False if request.args.get('action') is None else request.args.get('action')
-# 		if action != False and action == 'edit':
-# 			self._template_args['form'] = form
-# 			return super(SubjectTranscriptView_Teacher,self).index_view()		
-
-# 	# def render(self, template, **kwargs):
-# 	# 	if template == 'teacher/edit_score.html':
-# 	# 		kwargs['summary_data'] = [
-# 	# 			{'title': 'Page Total', 'name': None, 'cost': '1000'},
-# 	# 			{'title': 'Grand Total', 'name': None, 'cost': '2000'},
-# 	# 		]
-# 	# 	return super(SubjectTranscriptView_Teacher, self).render(template, **kwargs)
 
 
 
@@ -497,7 +415,7 @@ admin.add_view(EducationalOfficeView(EducationalOffice, db.session, name="Phòng
 
 admin.add_view(MyBaseView(Class, db.session, category="Quản lý lớp học", name="Thông tin lớp học"))
 admin.add_view(ClassInfoView(ClassInfo, db.session, category="Quản lý lớp học", name="Danh sách lớp học trong năm"))
-admin.add_view(MyBaseView(TeachingAssignment, db.session, category="Quản lý lớp học",name="Phân công giảng dạy"))
+admin.add_view(TeachingAssignmentView(TeachingAssignment, db.session, category="Quản lý lớp học",name="Phân công giảng dạy"))
 
 admin.add_view(MyBaseView(Subject, db.session, name="Quản lý môn học", menu_icon_type="ti", menu_icon_value="ti-book"))
 
@@ -517,3 +435,4 @@ admin.add_view(MyBaseView(Ethnic, db.session, category="Thông tin chung", name=
 
 from my_app.student import *
 from my_app.teacher import *
+from my_app.educational_office import *
