00cbdba277aa563f0c25ca4987492b403433ac71
Van Truong <school0123456@gmail.com>
2021-12-04 01:25:40 +0700

    Nho Thay doi duong dan app config database

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -11,7 +11,7 @@ import cloudinary.uploader
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:quang810@localhost/StudentManagementDB?charset=utf8mb4"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:123456789@localhost/StudentManagementDB?charset=utf8mb4"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ngvantruong:A!123456@ngvantruong.mysql.pythonanywhere-services.com/ngvantruong$ManagementStudent_db?charset=utf8mb4"
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=120)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
